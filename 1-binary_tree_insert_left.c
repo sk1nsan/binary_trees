956240689c72b807a5e297bbd43560265d867ab2
sk1nsan <138858455+sk1nsan@users.noreply.github.com>
2024-03-25 20:05:18 +0200

    1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -15,7 +15,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	binary_tree_t *new_node;
 
 	new_node = malloc(sizeof(binary_tree_t));
-	if (!new_node)
+	if (!new_node || !parent)
 	{
 		free(new_node);
 		return (NULL);
